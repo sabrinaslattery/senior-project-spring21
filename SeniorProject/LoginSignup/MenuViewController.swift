2c4bc2baaf5cdb558bccc7458b76043fe86c8db5
Wiliams Ordaz <Wiliams Ordaz>
2021-02-11 20:21:29 -0800

    I have added a Login/SignUp pages and some code that goes with it done exit

---
+++ SeniorProject/LoginSignup/MenuViewController.swift
@@ -0,0 +1,45 @@
+//
+//  File.swift
+//  SeniorProject
+//
+//  Created by William Ordaz on 2/11/21.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add the background gradient
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        get {
+            return .lightContent
+        }
+    }
+    
+}
