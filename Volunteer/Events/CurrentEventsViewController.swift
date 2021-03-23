94f02aa2e916068a1d3d1fce187eb56072f151e5
Sabrina Slattery <sabrina.slattery.noreply@gmail.com>
2021-03-22 19:11:28 -0700

    Minor Changes

---
+++ Volunteer/Events/CurrentEventsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CurrentEventsViewController.swift
+//  Volunteer
+//
+//  Created by Sabrina Slattery on 3/22/21.
+//
+
+import UIKit
+
+class CurrentEventsViewController: UIViewController {
+
+    @IBOutlet weak var createEventButton: UIButton!
+    
+    @IBOutlet weak var eventDate: UIImageView!
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventName: UILabel!
+    @IBOutlet weak var eventDifficulty: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
