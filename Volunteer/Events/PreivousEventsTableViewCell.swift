605dd43c66a41795a8cb72af6f934dd8b39da113
Sabrina Slattery <sabrina.slattery.noreply@gmail.com>
2021-03-23 14:07:09 -0700

    Re-created necessary swift files
    - table view and table cell view swift files for both current and previous events feeds

---
+++ Volunteer/Events/PreivousEventsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PreivousEventsTableViewCell.swift
+//  Volunteer
+//
+//  Created by Sabrina Slattery on 3/23/21.
+//
+
+import UIKit
+
+class PreivousEventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Volunteer/Events/PreviousEventsTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PreviousEventsTableViewController.swift
+//  Volunteer
+//
+//  Created by Sabrina Slattery on 3/23/21.
+//
+
+import UIKit
+
+class PreviousEventsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+}
