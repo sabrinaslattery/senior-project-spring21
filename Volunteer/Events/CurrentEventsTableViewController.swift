21b2c5a529c8756dc86cca035b3a72a9db5a1c4e
Sabrina Slattery <sabrina.slattery.noreply@gmail.com>
2021-03-23 14:20:31 -0700

    Minor changes
    - added starter code for backend table view swift files

--- Volunteer/Events/CurrentEventsTableViewController.swift
+++ Volunteer/Events/CurrentEventsTableViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class CurrentEventsTableViewController: UITableViewController {
 
+    var eventsArray = [NSDictionary]()
+    
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadEvents()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadEvents), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 150
     }
 
+    @objc func loadEvents() {
+        
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
@@ -20,6 +35,8 @@ class CurrentEventsTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        
+        //replace with eventsArray.count
         return 5
     }
 
--- Volunteer/Events/PreviousEventsTableViewController.swift
+++ Volunteer/Events/PreviousEventsTableViewController.swift
@@ -9,20 +9,34 @@ import UIKit
 
 class PreviousEventsTableViewController: UITableViewController {
 
+    var eventsArray = [NSDictionary]()
+    
+    let myRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadEvents()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadEvents), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 150
     }
-
-    // MARK: - Table view data source
+    
+    @objc func loadEvents() {
+        
+    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        
+        //replace with eventsArray.count
         return 3
     }
 
