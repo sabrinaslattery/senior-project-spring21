9f6fba5ac3fffd55eb610b0c6f226662fd78f422
JessicaAlvarez <Jessica.alvarez714@gmail.com>
2021-03-23 16:54:34 -0700

    Password Reset

--- Volunteer/SignUpLogin/ResetPassViewController.swift
+++ Volunteer/SignUpLogin/ResetPassViewController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import Parse
 
 class ResetPassViewController:UIViewController, UITextFieldDelegate {
     
@@ -18,7 +19,48 @@ class ResetPassViewController:UIViewController, UITextFieldDelegate {
         view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
         
     }
-    
+    @IBAction func SendLinkBtnAction(_ sender: Any) {
+             let emailAddress = enterEmailTextField.text!
+
+                   if emailAddress.isEmpty
+                   {
+                       //Display warning message
+                       let userMessage: String = "Please type in your email address"
+                       displayMessage(userMessage: userMessage)
+                       return
+                   }
+
+                   PFUser.requestPasswordResetForEmail(inBackground: emailAddress, block: { (success:Bool, error:Error?)in
+
+                       if(success)
+                       {
+                            //Display success message
+                        print("success")
+                           
+                       } else {
+                        //Display error message
+                      print(error!.localizedDescription)
+                        //CheckEmailViewController()
+                       }
+
+
+                   })
+         }
+
+         func displayMessage (userMessage:String)
+             {
+                     var myAlert = UIAlertController(title: "Missing Email", message: userMessage, preferredStyle: UIAlertController.Style.alert)
+
+                     let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) {
+                         action in
+                        // self.dismiss(animated: true, completion: nil)
+                     }
+
+                    myAlert.addAction(okAction)
+                    self.present(myAlert, animated: true, completion: nil)
+                     }
+
+
     
     @IBAction func handleDismissButton(_ sender: Any) {
         self.dismiss(animated: false, completion: nil)
