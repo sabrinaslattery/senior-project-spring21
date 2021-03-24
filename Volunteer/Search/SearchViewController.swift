266e66c8068dbb1dec9cdbeea862a3a5ea007a11
Stephen Baity <steve.baity@gmail.com>
2021-03-23 21:26:16 -0700

    Commit

---
+++ Volunteer/Search/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Volunteer
+//
+//  Created by Sabrina Slattery on 3/23/21.
+//
+
+import UIKit
+
+class SearchViewController: UISearchController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
