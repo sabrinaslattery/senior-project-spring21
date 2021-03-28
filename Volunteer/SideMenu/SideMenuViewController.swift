8b6d8e914dd1f559e5764156816aaa1e61d42e31
Sabrina Slattery <sabrina.slattery.noreply@gmail.com>
2021-03-28 12:43:26 -0700

    SideMenu Creation

---
+++ Volunteer/SideMenu/SideMenuViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SideMenuViewController.swift
+//  Volunteer
+//
+//  Created by Sabrina Slattery on 3/28/21.
+//
+
+import UIKit
+import Parse
+import SideMenu
+
+
+class SideMenuViewController: UISearchController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
