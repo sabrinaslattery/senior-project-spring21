99a305ef7d4ddb56b5857507f835a8d48a5b4212
Sabrina Slattery <sabrina.slattery.noreply@gmail.com>
2021-03-27 22:44:48 -0700

    Added Wiliams' Swift UI files
    - updated all 5 files in Events group

--- Volunteer/Events/CurrentEventsTableViewCell.swift
+++ Volunteer/Events/CurrentEventsTableViewCell.swift
@@ -9,20 +9,28 @@ import UIKit
 
 class CurrentEventsTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var eventDate: UIImageView!
-    @IBOutlet weak var eventDifficulty: UIImageView!
-    @IBOutlet weak var eventName: UILabel!
-    @IBOutlet weak var eventImage: UIImageView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var eventDate: UIDatePicker!
+        //@IBOutlet weak var eventDate: UILabel!
+        @IBOutlet weak var eventTags: UILabel!
+        @IBOutlet weak var eventDifficulty: UILabel!
+        
+        
+        //@IBOutlet weak var eventDate: UIDatePicker!
+        //@IBOutlet weak var eventTags: UIPickerView!
+        //@IBOutlet weak var eventDifficulty: UIPickerView!
+        
+        @IBOutlet weak var eventName: UILabel!
+        @IBOutlet weak var eventImage: UIImageView!
+        
+        override func awakeFromNib() {
+            super.awakeFromNib()
+            // Initialization code
+        }
+
+        override func setSelected(_ selected: Bool, animated: Bool) {
+            super.setSelected(selected, animated: animated)
+
+            // Configure the view for the selected state
+        }
+
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-}
--- Volunteer/Events/PreviousEventsTableViewCell.swift
+++ Volunteer/Events/PreviousEventsTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  PreivousEventsTableViewCell.swift
+//  PreviousEventsTableViewCell.swift
 //  Volunteer
 //
 //  Created by Sabrina Slattery on 3/23/21.
@@ -9,20 +9,28 @@ import UIKit
 
 class PreviousEventsTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var eventDate: UIImageView!
-    @IBOutlet weak var eventName: UILabel!
-    @IBOutlet weak var eventDifficulty: UIImageView!
-    @IBOutlet weak var eventImage: UIImageView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+     @IBOutlet weak var eventDate: UIDatePicker!
+     //@IBOutlet weak var eventDate: UILabel!
+     @IBOutlet weak var eventTags: UILabel!
+     @IBOutlet weak var eventDifficulty: UILabel!
+     
+     
+     //@IBOutlet weak var eventDate: UIDatePicker!
+     //@IBOutlet weak var eventTags: UIPickerView!
+     //@IBOutlet weak var eventDifficulty: UIPickerView!
+     
+     @IBOutlet weak var eventName: UILabel!
+     @IBOutlet weak var eventImage: UIImageView!
+     
+     override func awakeFromNib() {
+         super.awakeFromNib()
+         // Initialization code
+     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+     override func setSelected(_ selected: Bool, animated: Bool) {
+         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
-    }
+         // Configure the view for the selected state
+     }
 
-}
+ }
