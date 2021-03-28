4ebfb57f5fcc956960531a773f50e3704673054b
Sabrina Slattery <sabrina.slattery.noreply@gmail.com>
2021-03-28 14:04:29 -0700

    Added beta side menu storyboard
    - Began testing a beta side menu, still some errors

--- Volunteer/SideMenu/SideMenuViewController.swift
+++ Volunteer/SideMenu/SideMenuViewController.swift
@@ -9,24 +9,49 @@ import UIKit
 import Parse
 import SideMenu
 
-
 class SideMenuViewController: UISearchController {
 
+    var menu: SideMenuNavigationController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        menu = SideMenuNavigationController(rootViewController: UIViewController())
+        menu?.leftSide = true
+        
+        SideMenuManager.default.leftMenuNavigationController = menu
+        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTapMenu() {
+        present(menu!, animated: true)
     }
-    */
+}
 
+
+class MenuListController: UITableViewController {
+    
+    var itemsArray = ["User's Name", "", "Home", "My Profile", "Events", "Create an Event", "Search", "Settings", "", "Sign Out"]
+    
+    let sideBarColor = UIColor(red: 75/225.0, green: 75/225.0, blue: 75/225.0, alpha: 1)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = sideBarColor
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = itemsArray[indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = sideBarColor
+        
+        return cell
+    }
 }
