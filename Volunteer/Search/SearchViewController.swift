a7c8db8f8cfaa680b7a551a750304169d894b17d
BobFisherman18 <46041494+BobFisherman18@users.noreply.github.com>
2021-03-29 20:15:16 -0700

    Added a data model to display in the the Search View Controller: Changed UIScrollView in Search storyboard to a Table View. There is an error on search bar even though i connected it from the storyboard. Added dummy data (Will replace soon as I needed a API key from One Degree). Here is the the website I am grabbing for more info: https://www.1degree.org.

--- Volunteer/Search/SearchViewController.swift
+++ Volunteer/Search/SearchViewController.swift
@@ -7,12 +7,58 @@
 
 import UIKit
 
-class SearchViewController: UISearchController {
+class SearchViewController: UISearchController, UITableViewDelegate, UISearchBarDelegate {
 
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+    
+    //dummy data
+    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
+            "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
+            "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
+            "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
+            "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
+
+        var filteredData: [String]!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        tableView.delegate = self
 
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as UITableViewCell
+            cell.textLabel?.text = filteredData[indexPath.row]
+            return cell
+        }
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return filteredData.count
+        }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+           // When there is no text, filteredData is the same as the original data
+           // When user has entered text into the search box
+           // Use the filter method to iterate over all items in the data array
+           // For each item, return true if the item should be included and false if the
+           // item should NOT be included
+           filteredData = searchText.isEmpty ? data : data.filter { (item: String) -> Bool in
+               // If dataItem matches the searchText, return true to include it
+               return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+           }
+           
+           tableView.reloadData()
     }
 
 }
