cc63271571de871ad46a48ae118bf845852ad05e
Josh <josh.freedman89@gmail.com>
2021-03-30 12:11:28 -0700

    edit profile updated

---
+++ Volunteer/Home/AboutUsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AboutUsViewController.swift
+//  Volunteer
+//
+//  Created by Joshua Freedman on 3/29/21.
+//
+
+import UIKit
+import SafariServices
+
+class AboutUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonTapped() {
+        
+        let vc = SFSafariViewController(url: URL(string: "https://wearecolorblind.com/")!)
+        
+        present(vc, animated:true)
+    }
+ 
+}
