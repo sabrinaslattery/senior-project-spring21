a49fc8e596c373b68836b7382a50e9f31efa8ea5
BobFisherman18 <46041494+BobFisherman18@users.noreply.github.com>
2021-04-09 17:34:44 -0700

    Fixed an issue when data included placeholder text in the Search Event Cell Swift file.

--- Volunteer/Search/SearchViewController.swift
+++ Volunteer/Search/SearchViewController.swift
@@ -20,16 +20,9 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
     
     @IBOutlet var tableView: UITableView!
     
-    //dummy data; will replace soon
-    /*
-    let data = ["New York, NY", "Los Angeles, CA", "Chicago, IL", "Houston, TX",
-            "Philadelphia, PA", "Phoenix, AZ", "San Diego, CA", "San Antonio, TX",
-            "Dallas, TX", "Detroit, MI", "San Jose, CA", "Indianapolis, IN",
-            "Jacksonville, FL", "San Francisco, CA", "Columbus, OH", "Austin, TX",
-            "Memphis, TN", "Baltimore, MD", "Charlotte, ND", "Fort Worth, TX"]
-*/
-        var events = [PFObject]()
-        var filteredData: [String]!
+   
+    var events = [PFObject]()
+    var filteredData: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,37 +30,49 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         searchBar.delegate = self
         
-        //filteredData = data commented out for now
+         
         // Do any additional setup after loading the view.
     }
     
     
     override func viewDidAppear(_ animated: Bool) {
-           super.viewDidAppear(animated)
+        super.viewDidAppear(animated)
            
-           let query = PFQuery(className: "Events")
+        let query = PFQuery(className: "Events")
         query.includeKey("title")
-           query.limit = 20
+           query.limit = 100
            
            query.findObjectsInBackground { (events, error) in
-               if events != nil {
+            if events != nil {
                    self.events = events!
                    self.tableView.reloadData()
                }
            }
        }
-
+    
+    
+    // MARK: - Table view data source
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.count
+        let event = events[section]
+        
+        //?? operator gives PFObject a default value, in this case "[]"
+        let eventData = (event["title"] as? [PFObject]) ?? []
+        
+        
+           
+        
+        //adds the other two table view cells
+       return eventData.count + 1
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
            return events.count
-       }
+      }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let event = events[indexPath.section]
+        
         
         if indexPath.row == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "SearchEventCell", for: indexPath) as! SearchEventCell
@@ -77,28 +82,37 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
 
         
             return cell
-        }
-        else {
-                    let cell = tableView.dequeueReusableCell(withIdentifier: "SearchEventCell")!
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SearchEventCell")!
                     
-                    return cell
+            return cell
                 }
     }
     
-    // This method updates filteredData based on the text in the Search Box
-    /* remove comments later
-      func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-          // When there is no text, filteredData is the same as the original data
-          // When user has entered text into the search box
-          // Use the filter method to iterate over all items in the data array
-          // For each item, return true if the item should be included and false if the
-          // item should NOT be included
-          filteredData = searchText.isEmpty ? data : data.filter { (item: String) -> Bool in
-              // If dataItem matches the searchText, return true to include it
-              return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
-          }
-     */
-          
+    // This method updates filteredData based on the text in the Search Box;
+    //search method does not work yet
+  /*
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // When there is no text, filteredData is the same as the original data
+        // When user has entered text into the search box
+        // Use the filter method to iterate over all items in the data array
+        // For each item, return true if the item should be included and false if the
+        // item should NOT be included
+        
+        let event = events(className: "Events")
+        let eventTitle = event["title"] as? String
+        
+        
+
+        filteredData = searchText.isEmpty ? eventTitle : eventTitle.filter { (item: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        tableView.reloadData()
+    }
+    */
+
          
-      
+   
 }
