1cd81443c5497699ad04e8fef130c7fa9db9c7d5
Sabrina Slattery <sabrina.slattery.noreply@gmail.com>
2021-04-23 12:26:35 -0700

    Reformat Settings, Created Update Email Page

---
+++ Volunteer/Events/UpdateEmailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UpdateEmailViewController.swift
+//  Volunteer
+//
+//  Created by Sabrina Slattery on 4/23/21.
+//
+
+import UIKit
+
+class UpdateEmailViewController: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var currentEmail: UILabel!
+    @IBOutlet weak var newEmail: UITextField!
+    @IBOutlet weak var confirmNewEmail: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
