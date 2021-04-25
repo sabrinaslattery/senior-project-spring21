6c58e4090e58fb599cd262c9234f9945c50a79e5
sandovro <47549509+sandovro@users.noreply.github.com>
2021-04-24 20:28:07 -0700

    Change Password
    Progress for change password view controller. There are no errors with the password view and swift code

--- Volunteer/ChangePasswordViewController.swift
+++ Volunteer/ChangePasswordViewController.swift
@@ -5,22 +5,21 @@
 //  Created by Roberto on 4/24/21.
 //
 
-import Foundation
 import UIKit
 import Parse
 
 class ChangePasswordViewController: UIViewController {
     
-    @IBOutlet weak var currentPassword: UILabel!
-    @IBOutlet weak var newPassword: UILabel!
-    @IBOutlet weak var newPasswordConfirm: UILabel!
+    @IBOutlet weak var currentPassword: UITextField!
+    @IBOutlet weak var newPassword: UITextField!
+    @IBOutlet weak var newPasswordConfirm: UITextField!
     @IBOutlet weak var submitButton: UIButton!
+    
     
     let user = PFUser()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -39,8 +38,8 @@ class ChangePasswordViewController: UIViewController {
             myAlert.addAction(okAction);
             self.present(myAlert, animated:true, completion:nil);
         }
-        else if newPassword.text == "" || newPasswordConfirm.text == "" {
-            let userMessage = "Cannot submit null or mismatching fields."
+        else if !currentPassword.hasText || !newPassword.hasText || !newPasswordConfirm.hasText {
+            let userMessage = "Cannot Submit Empty or Mismatching Fields."
             let myAlert = UIAlertController(title:"Please try again.", message:userMessage, preferredStyle: UIAlertController.Style.alert);
                             
             let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
@@ -56,23 +55,23 @@ class ChangePasswordViewController: UIViewController {
                     result!["password"] = self.newPassword.text
                     result?.saveInBackground() {
                         (success: Bool, error: Error?) in
-                            if (success) {
-                                // The object has been saved.
-                            let userMessage = "Password Was Successfully Updated."
-                            let myAlert = UIAlertController(title:"Success!", message:userMessage, preferredStyle: UIAlertController.Style.alert);
-                                
-                            let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { (action:UIAlertAction!) -> Void in
-                                self.performSegue(withIdentifier: "returnToSettings", sender: nil)
-                                })
+                        if (success) {
+                            // The object has been saved.
+                        let userMessage = "Password Was Successfully Updated."
+                        let myAlert = UIAlertController(title:"Success!", message:userMessage, preferredStyle: UIAlertController.Style.alert);
+                            
+                        let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { (action:UIAlertAction!) -> Void in
+                            self.performSegue(withIdentifier: "returnToSettings", sender: nil)
+                            })
+                        myAlert.addAction(okAction);
+                        self.present(myAlert, animated:true, completion:nil);
+                        }
+                        else {
+                            let userMessage = "There Was an Error"
+                            let myAlert = UIAlertController(title:"Please Try Again.", message:userMessage, preferredStyle: UIAlertController.Style.alert);
+                            let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
                             myAlert.addAction(okAction);
                             self.present(myAlert, animated:true, completion:nil);
-                            }
-                            else {
-                                let userMessage = "There Was an Error"
-                                let myAlert = UIAlertController(title:"Please Try Again.", message:userMessage, preferredStyle: UIAlertController.Style.alert);
-                                let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
-                                myAlert.addAction(okAction);
-                                self.present(myAlert, animated:true, completion:nil);
                             }
                     }
                 }
@@ -80,6 +79,4 @@ class ChangePasswordViewController: UIViewController {
         }
     }
 
-
 }
-    
