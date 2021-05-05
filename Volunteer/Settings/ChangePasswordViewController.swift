28b9d4d498dec46991214069d707f32a48bb736a
sandovro <47549509+sandovro@users.noreply.github.com>
2021-05-05 16:23:57 -0700

    Change Password Redo
    I've redone the Change Password section

--- Volunteer/Settings/ChangePasswordViewController.swift
+++ Volunteer/Settings/ChangePasswordViewController.swift
@@ -10,11 +10,8 @@ import Parse
 
 class ChangePasswordViewController: UIViewController {
     
-    @IBOutlet weak var currentPassword: UITextField!
-    @IBOutlet weak var newPassword: UITextField!
-    @IBOutlet weak var newPasswordConfirm: UITextField!
+    @IBOutlet weak var userEmailEntered: UITextField!
     @IBOutlet weak var submitButton: UIButton!
-    
     
     let user = PFUser()
     
@@ -24,58 +21,56 @@ class ChangePasswordViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        newPassword.resignFirstResponder()
-        newPasswordConfirm.resignFirstResponder()
         NotificationCenter.default.removeObserver(self)
     }
     
+
+    
     @IBAction func submitForm(_ sender: Any) {
-        if  (self.newPassword.text != self.newPasswordConfirm.text && self.currentPassword.text != user.password) {
-            let userMessage = "Passwords Need to Match."
-            let myAlert = UIAlertController(title:"Please Try Again", message:userMessage, preferredStyle: UIAlertController.Style.alert);
+        
+        let emailAddressEntered = userEmailEntered.text!
+        //let userEmailActual = user.email
+
+        if emailAddressEntered.isEmpty {
+            
+            let userMessage = "Email Field Cannot Be Empty."
+            let myAlert = UIAlertController(title:"Please try again", message:userMessage, preferredStyle: UIAlertController.Style.alert);
                             
             let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
             myAlert.addAction(okAction);
             self.present(myAlert, animated:true, completion:nil);
         }
-        else if !currentPassword.hasText || !newPassword.hasText || !newPasswordConfirm.hasText {
-            let userMessage = "Cannot Submit Empty or Mismatching Fields."
-            let myAlert = UIAlertController(title:"Please try again.", message:userMessage, preferredStyle: UIAlertController.Style.alert);
-                            
-            let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
-            myAlert.addAction(okAction);
-            self.present(myAlert, animated:true, completion:nil);
-        }
-        else {
-            let query = PFQuery(className: "User")
-            query.whereKey("user", equalTo: PFUser.current()!)
-            query.getFirstObjectInBackground { [self] (object, error) -> Void in
-                if error == nil {
-                    let result = object
-                    result!["password"] = self.newPassword.text
-                    result?.saveInBackground() {
-                        (success: Bool, error: Error?) in
-                        if (success) {
-                            // The object has been saved.
-                        let userMessage = "Password Was Successfully Updated."
-                        let myAlert = UIAlertController(title:"Success!", message:userMessage, preferredStyle: UIAlertController.Style.alert);
-                            
-                        let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { (action:UIAlertAction!) -> Void in
-                            self.performSegue(withIdentifier: "returnToSettings", sender: nil)
-                            })
-                        myAlert.addAction(okAction);
-                        self.present(myAlert, animated:true, completion:nil);
-                        }
-                        else {
-                            let userMessage = "There Was an Error"
-                            let myAlert = UIAlertController(title:"Please Try Again.", message:userMessage, preferredStyle: UIAlertController.Style.alert);
-                            let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
-                            myAlert.addAction(okAction);
-                            self.present(myAlert, animated:true, completion:nil);
-                            }
-                    }
+//        else if  (emailAddressEntered != userEmailActual) {
+//            let userMessage = "Email must match the one in your account."
+//            let myAlert = UIAlertController(title:"Please try again", message:userMessage, preferredStyle: UIAlertController.Style.alert);
+//
+//            let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
+//            myAlert.addAction(okAction);
+//            self.present(myAlert, animated:true, completion:nil);
+//        }
+        else{
+        
+            PFUser.requestPasswordResetForEmail(inBackground: emailAddressEntered, block: { (success:Bool, error:Error?)in
+                if(success){
+                    
+                    let userMessage = "An email with instructions to reset you password has been sent"
+                    let myAlert = UIAlertController(title:"Success!", message:userMessage, preferredStyle: UIAlertController.Style.alert);
+                    let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
+                        
+//                    let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: { (action:UIAlertAction!) -> Void in
+//                        DispatchQueue.main.async { self.performSegue(withIdentifier: "returnToSettings", sender: self)}
+//                        })
+                    myAlert.addAction(okAction);
+                    self.present(myAlert, animated:true, completion:nil);
+                               
+                } else {
+                    let userMessage = "There was an error"
+                    let myAlert = UIAlertController(title:"Please try again.", message:userMessage, preferredStyle: UIAlertController.Style.alert);
+                    let okAction =  UIAlertAction(title: "Ok", style: UIAlertAction.Style.default)
+                    myAlert.addAction(okAction);
+                    self.present(myAlert, animated:true, completion:nil);
                 }
-        }
+            })
         }
     }
 
